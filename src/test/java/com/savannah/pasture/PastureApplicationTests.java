919d286f2d2a62aba4ff49a846525e97ed304519
GiwoongBang <107822628+GiwoongBang@users.noreply.github.com>
2024-04-14 17:16:01 +0900

    first commit

---
+++ src/test/java/com/savannah/pasture/PastureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savannah.pasture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
